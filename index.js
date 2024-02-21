61da4289a9856df757da0b7ceea29307036f82ec
Jayashrikothandapani <jayashrikgokul@gmail.com>
2024-02-21 08:43:16 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,25 @@
+//offer bar
+var offerbar = document.querySelector(".offerbar")
+var x = document.getElementById("x")
+x.addEventListener("click",function(){
+    offerbar.style.display = "none"
+})
+
+
+//menubar
+var menubar = document.querySelector(".menubar")
+var menubaritems = document.querySelector(".menubaritems")
+menubar.addEventListener("click",function(){
+    menubaritems.style.display="block"
+    
+})
+
+
+
+var menubaritems = document.querySelector(".menubaritems")
+var p = document.getElementById("p")
+p.addEventListener("click",function(){
+    menubaritems.style.display = "none"
+})
+
+
